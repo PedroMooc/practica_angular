fd290399ed71ff83d6613f14d3d51dd45a7709fb
Pedro Morales <pedro.morales.ochoa@gmail.com>
2024-07-17 12:25:46 -0600

    Módulos en angular

--- vite/02-bases/src/app/app.module.ts
+++ vite/02-bases/src/app/app.module.ts
@@ -3,20 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero.component';
-import { ListComponent } from './list/list.component';
+import { ListComponent } from './heroes/list/list.component';
+import { CounterModeule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
     HeroComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModeule
   ],
   providers: [],
   bootstrap: [AppComponent]
