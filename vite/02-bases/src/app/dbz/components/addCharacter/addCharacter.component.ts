fc70b4789fd2b66052897d28795cddc98c271b5d
Pedro Morales <pedro.morales.ochoa@gmail.com>
2024-07-18 13:22:53 -0600

    @Input() - Recibir del padre

---
+++ vite/02-bases/src/app/dbz/components/addCharacter/addCharacter.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character',
+  templateUrl: './addCharacter.component.html',
+  styleUrl: './addCharacter.component.css',
+})
+export class AddCharacterComponent { }
--- vite/02-bases/src/app/dbz/dbz.module.ts
+++ vite/02-bases/src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/addCharacter/addCharacter.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   exports:[
     MainPageComponent
