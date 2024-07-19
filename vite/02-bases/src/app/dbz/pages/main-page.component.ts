973f73ac3dcd20bf205f79a5b876be87c349ea85
Pedro Morales <pedro.morales.ochoa@gmail.com>
2024-07-19 17:35:49 -0600

    @OutPut() - emitir eventos al padre

--- vite/02-bases/src/app/dbz/pages/main-page.component.ts
+++ vite/02-bases/src/app/dbz/pages/main-page.component.ts
@@ -9,16 +9,23 @@ import { Character } from '../interfaces/character.interface';
 
 export class MainPageComponent {
   public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },
-  {
-    name: 'Goku',
-    power: 9500
-  },
-  {
-    name: 'Vegeta',
-    power: 7500
+      name: 'Krillin',
+      power: 1000
+    },
+    {
+      name: 'Goku',
+      power: 9500
+    },
+    {
+      name: 'Vegeta',
+      power: 7500
+    }
+  ];
+
+  onNewCharacter(character: Character): void{
+     console.log('MainPage');
+     console.log(character);
   }
-];
 }
+
+
