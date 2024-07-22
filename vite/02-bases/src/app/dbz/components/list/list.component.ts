f648fbc3bfdcaf2c17979861fd817eab89b873bf
Pedro Morales <pedro.morales.ochoa@gmail.com>
2024-07-22 13:24:20 -0600

    Borrado de elementos utilizando evento output

--- vite/02-bases/src/app/dbz/components/list/list.component.ts
+++ vite/02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {  Component, Input } from '@angular/core';
+import {  Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 
@@ -13,4 +13,14 @@ export class ListComponent {
 
   @Input()
   public characterList: Character[] = [];
+
+  @Output()
+  public onDeleteId: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number): void{
+    //TODO:  Emitir Id de pesonaje
+    console.log({index});
+    this.onDeleteId.emit(index);
+
+  }
 }
