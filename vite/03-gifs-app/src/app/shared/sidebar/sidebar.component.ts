3401ab445f3d55cbda381727891d30eb3a420516
Pedro Morales <pedro.morales.ochoa@gmail.com>
2024-07-30 13:48:59 -0600

    Resolución de tarea de busqueda con historial de sidebar

--- vite/03-gifs-app/src/app/shared/sidebar/sidebar.component.ts
+++ vite/03-gifs-app/src/app/shared/sidebar/sidebar.component.ts
@@ -14,8 +14,11 @@ export class SidebarComponent {
 
    }
 
-  get tags() {
+  get tags(): string[] {
     return this.gifsService.tagsHistory;
+  }
+  searchTag(tag: string):void{
+    this.gifsService.searchTag(tag);
   }
 
 
