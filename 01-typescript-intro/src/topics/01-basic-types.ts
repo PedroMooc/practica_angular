f05755f830332c5063b8cbaadc666ddb16f33ce7
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-03-04 11:49:22 -0600

    01 - basic types

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,14 @@
+const name: string = 'Strider';
+let hpPoints: number | 'FULL' = 95;
+const isAlive: boolean = true;
+
+
+
+console.log('01-basic-types');
+console.log(
+    name, hpPoints, isAlive
+);
+
+
+
+export {};
