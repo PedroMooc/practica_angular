069b8706e95239ea1a552d7000477193681773a6
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-11-04 06:12:12 -0600

    proyecto001 - interpolación en los archivos HTML de angular

--- angular-ya/proyecto001/src/app/app.component.ts
+++ angular-ya/proyecto001/src/app/app.component.ts
@@ -10,4 +10,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'proyecto001';
+  nombre = 'Pedro Morales';
+  edad = 30;
+  email = 'pedro@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  sitio = 'www.google.com';
+
+  esActivo() {
+    if (this.activo) {
+      return 'Trabajador Activo';
+    }else {
+      return 'Trabajador Inactivo';
+    }
+  }
+
+  ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++) {
+      suma += this.sueldos[x];
+    }
+    return suma;
+  }
 }
