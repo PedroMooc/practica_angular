9c65f884e2fd8e5fd1c01fc2e5da0f40f673c0c9
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-11-12 13:15:40 -0600

    proyecto006 - Componentes: llamar a métodos del componente hijo desde el template del padre

---
+++ angular-ya/proyecto006/src/app/selectornumerico/selectornumerico.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-selectornumerico',
+  standalone: true,
+  imports: [],
+  templateUrl: './selectornumerico.component.html',
+  styleUrl: './selectornumerico.component.css'
+})
+export class SelectornumericoComponent {
+  @Input() minimo: number = 1;
+  @Input() maximo: number = 1;
+  actual: number = 1;
+
+  ngOnInit() {
+    this.actual = this.minimo;
+  }
+
+  incrementar() {
+    if (this.actual < this.maximo) {
+      this.actual++;
+    }
+  }
+
+  decrementar() {
+    if (this.actual > this.minimo) {
+      this.actual--;
+    }
+  }
+
+  fijar(v: number) {
+    if(v>=this.minimo && v<=this.maximo){
+      this.actual = v;
+    }
+
+  }
+}
