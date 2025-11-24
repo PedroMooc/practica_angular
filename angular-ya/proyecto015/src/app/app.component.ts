92769aa6c4c0f1b22a1e05ee56ea5cc336aa9427
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-11-24 17:55:26 -0600

    proyecto015 - pipes: creación de pipes personalizadas

---
+++ angular-ya/proyecto015/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LetrasPipe } from './letras.pipe';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    LetrasPipe,
+    CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'proyecto015';
+  vector = [1,2,3,4,5,6,7];
+}
