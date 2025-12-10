d4759fbb9f790f9165d0b4cb9aec1bc5cfd0a508
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-12-10 17:35:13 -0600

    proyecto041 - se consume componente de tabla desde app.component

--- angular-ya/proyecto041/src/app/app.component.ts
+++ angular-ya/proyecto041/src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostblogService } from './postblog.service';
+import { TablaComponent } from "./tabla/tabla.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule,
+    TablaComponent,
+    NgIf
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto041';
+  post: any;
+  titulosColumnas = [
+    'código de usuario',
+    'Código de post',
+    'Titulo',
+    'Descripción'
+  ];
+
+  constructor(private postblogService: PostblogService){
+
+  }
+
+  ngOnInit() {
+    this.postblogService.retornar()
+      .subscribe(result => this.post = result)
+  }
+
 }
