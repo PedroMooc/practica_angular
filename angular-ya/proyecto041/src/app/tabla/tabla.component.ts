6894d181f48c1af03417b6fc2adae7b218f5bae1
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-12-10 17:16:33 -0600

    Modificación del archivo tabla.component

--- angular-ya/proyecto041/src/app/tabla/tabla.component.ts
+++ angular-ya/proyecto041/src/app/tabla/tabla.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'boot-tabla',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor
+  ],
   templateUrl: './tabla.component.html',
   styleUrl: './tabla.component.css'
 })
 export class TablaComponent {
+  @Input() titulos: any;
+  @Input() filas: any;
 
+  objectKeys = Object.keys;
+
+  constructor (){}
+
+  ngOnInit() {
+
+  }
 }
