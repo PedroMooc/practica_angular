a6eab1477ce8f524014e3ab684bf2dfb07d8f75d
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-12-11 08:09:07 -0600

    proyecto043 - se consume el servicio desde el componente principal

--- angular-ya/proyecto043/src/app/app.component.ts
+++ angular-ya/proyecto043/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NgbModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
