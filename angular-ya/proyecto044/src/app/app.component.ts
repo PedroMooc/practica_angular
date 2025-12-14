c5cff07e5a1036427662a1512152a30cbcb734e6
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-12-13 20:32:44 -0600

    proyecto045 - (Modificaciones a proyecto 44 y 45)

--- angular-ya/proyecto044/src/app/app.component.ts
+++ angular-ya/proyecto044/src/app/app.component.ts
@@ -1,26 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { NgSwitch,NgSwitchCase, NgSwitchDefault } from "@angular/common";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    FormsModule,
-    NgSwitch,
-    NgSwitchCase,
-    NgSwitchDefault
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto044';
-  valor1!: number;
-  valor2!: number;
-  operacion: string = "ninguna";
-  resultado!: number;
+
 
 }
--- angular-ya/proyecto045/src/app/app.component.ts
+++ angular-ya/proyecto045/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgSwitch,NgSwitchCase, NgSwitchDefault } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto045';
+    valor1!: number;
+  valor2!: number;
+  operacion: string = "ninguna";
+  resultado!: number;
 }
