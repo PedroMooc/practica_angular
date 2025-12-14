9f179ce012501d6e1f722a651d1f1e14edd59623
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-12-13 20:31:59 -0600

    proyecto045 - Directivas estructurales ngSwitchDefault y ngSwitch

--- angular-ya/proyecto044/src/app/app.component.ts
+++ angular-ya/proyecto044/src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgSwitch,NgSwitchCase, NgSwitchDefault } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'proyecto044';
+  valor1!: number;
+  valor2!: number;
+  operacion: string = "ninguna";
+  resultado!: number;
+
 }
