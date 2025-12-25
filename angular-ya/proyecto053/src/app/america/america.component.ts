de8362598e003b5e000b2fed2626c2833b5fe927
Pedro Morales <pedro.morales.ochoa@gmail.com>
2025-12-25 06:59:53 -0600

    proyecto053 - router - rutas anidadas

---
+++ angular-ya/proyecto053/src/app/america/america.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-america',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './america.component.html',
+  styleUrl: './america.component.css'
+})
+export class AmericaComponent {
+
+}
---
+++ angular-ya/proyecto053/src/app/europa/europa.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-europa',
+  standalone: true,
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './europa.component.html',
+  styleUrl: './europa.component.css'
+})
+export class EuropaComponent {
+
+}
